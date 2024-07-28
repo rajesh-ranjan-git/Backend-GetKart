4d2551b07e9290b558e2b2c734941e1e74b1afc6
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-07-28 19:39:47 +0530

    cors implemented

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dbConnect from "./dbConnect.js";
 import usersRouter from "./Routers/users.routers.js";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
+import cors from "cors";
 
 const server = express();
 
@@ -11,6 +12,7 @@ let PORT = process.env.PORT || 3000;
 server.use(express.json());
 server.use(urlencoded({ extended: true }));
 server.use(cookieParser());
+server.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 server.use("/users", usersRouter);
 
